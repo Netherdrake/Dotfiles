feefa350b949d3f2c82c96787ddb088359ab4a58
Furion <_@furion.me>
2013-06-17 17:33:12 +0200

    known hosts

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 cp -f .vimrc ~/.vimrc
+cp -f config ~/.ssh/config
 cp -f .gitignore ~/.gitignore
 cp -f .editorconfig ../.editorconfig
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 cp -f ~/.vimrc .vimrc
+cp -f ~/.ssh/config config
 cp -f ../.editorconfig .editorconfig
 cp -f ~/.gitignore .gitignore
