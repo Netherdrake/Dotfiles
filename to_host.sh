3465d531dfa76f33276634d68d811a82a0f02358
Jan Netherdrake <me@janbirsa.com>
2013-07-03 02:18:10 +0200

    added tmux

--- to_host.sh
+++ to_host.sh
@@ -1,4 +1,5 @@
 cp -f .vimrc ~/.vimrc
+cp -f .tmux.conf ~/.tmux.conf 
 cp -f config ~/.ssh/config
 cp -f .gitignore ~/.gitignore
 cp -f .editorconfig ../.editorconfig
--- to_repo.sh
+++ to_repo.sh
@@ -1,4 +1,5 @@
 cp -f ~/.vimrc .vimrc
+cp -f ~/.tmux.conf .tmux.conf
 cp -f ~/.ssh/config config
 cp -f ../.editorconfig .editorconfig
 cp -f ~/.gitignore .gitignore
