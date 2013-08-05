193b9d91e57d695d1116368161f77e57cbf1d0c7
Jan Netherdrake <me@janbirsa.com>
2013-08-06 00:55:30 +0200

    added tmux to .sh scripts

--- to_host.sh
+++ to_host.sh
@@ -1,5 +1,6 @@
 cp -f .vimrc ~/.vimrc
-cp -f .tmux.conf ~/.tmux.conf 
+cp -f .tmux.conf ~/.tmux.conf
+cp -f ./tmux/default.sh ~/.tmux/tmux-powerline/themes/default.sh
 cp -f config ~/.ssh/config
 cp -f .gitignore ~/.gitignore
 cp -f .editorconfig ../.editorconfig
--- to_repo.sh
+++ to_repo.sh
@@ -1,5 +1,6 @@
 cp -f ~/.vimrc .vimrc
 cp -f ~/.tmux.conf .tmux.conf
+cp -f ~/.tmux/tmux-powerline/themes/default.sh ./tmux/default.sh
 cp -f ~/.ssh/config config
 cp -f ../.editorconfig .editorconfig
 cp -f ~/.gitignore .gitignore
