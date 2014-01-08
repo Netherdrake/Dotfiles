fdd7c540e0569b0003b93f545e7b3351fec9fce8
Jan Netherdrake <me@janbirsa.com>
2014-01-08 14:50:42 -0700

    added fish config

---
+++ config.fish
@@ -0,0 +1,12 @@
+set fish_greeting ""
+
+function fish_prompt
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+  echo -n ' % '
+end
+
+function lockme
+  /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+end
