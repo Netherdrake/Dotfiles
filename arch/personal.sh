12104e948a11faddef2fbc946734e6c705e3cc11
Furion <me@janbirsa.com>
2016-07-10 04:48:36 +0200

    minor tweak

--- arch/personal.sh
+++ arch/personal.sh
@@ -3,5 +3,6 @@
 ## Install Packages
 sudo pacman -Syu
 sudo pacman -Sy --needed snapd speedcrunch gimp vlc kdegraphics-okular youtube-dl
-sudo pacman -Sy --needed dropbox 
-yaourt -Sy --needed spotify soundnode-app-bin redshift gyazo nautilus-dropbox mattermost-desktop
+sudo pacman -Sy --needed dropbox
+yaourt -Sy --needed spotify redshift gyazo nautilus-dropbox mattermost-desktop
+
