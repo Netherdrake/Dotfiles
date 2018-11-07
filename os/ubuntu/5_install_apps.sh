66b30c29bbe8bbc8684520029d33d4ab76a2234e
Furion <_@furion.me>
2018-11-07 21:55:15 +0100

    Add Typora app

--- os/ubuntu/5_install_apps.sh
+++ os/ubuntu/5_install_apps.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 # Typora
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+
+# install typora
+sudo apt-get install typora
+
 # Etcher
