bffccba6ac4fc4ab38beb1445810a1238d14333d
Furion <_@furion.me>
2018-11-11 01:16:45 +0100

    Add jq too utils installer

--- os/ubuntu/1_install_utils.sh
+++ os/ubuntu/1_install_utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Common utilities
-sudo apt install -y tree curl wget htop ufw gufw cryptsetup encfs
+sudo apt install -y tree curl wget htop ufw gufw cryptsetup encfs jq
 #sudo add-apt-repository ppa:gencfsm && sudo apt update && sudo apt install -y gnome-encfs-manager
 pip3 install youtube-dl
 
