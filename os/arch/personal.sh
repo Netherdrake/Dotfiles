a7d2d48e44557377cabc6a7388e25681725318d5
Furion <_@furion.me>
2020-02-18 00:52:09 +0100

    Remove obsolete Arch linux scripts

--- os/arch/personal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Install Packages
-sudo pacman -Syu
-sudo pacman -Sy --needed snapd speedcrunch gimp vlc kdegraphics-okular youtube-dl
-# sudo pacman -Sy --needed dropbox
-yaourt -Sy --needed spotify redshift gyazo
-
--- os/arch/privacy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Install Packages
-sudo pacman -Syu
-sudo pacman -Sy --needed truecrypt veracrypt encfs keepassx2 thunderbird
-yaourt -Sy --needed thunderbird-enigmail
