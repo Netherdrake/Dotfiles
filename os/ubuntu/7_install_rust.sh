61b2adc45fd28f8a344178e6d9c73cb6aced919c
Netherdrake <user.email>
2022-10-19 16:00:59 +0200

    Rust nightly config

--- os/ubuntu/7_install_rust.sh
+++ os/ubuntu/7_install_rust.sh
@@ -7,8 +7,18 @@ cargo xtask install --server
 cd ..
 rm -rf rust-analyzer
 
+rustup component add rust-src
 rustup component add rustfmt
 rustup component add clippy
 
 cargo install cargo-watch
 cargo install evcxr
+
+rustup install nightly
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+rustup component add rust-analyzer --toolchain nightly-x86_64-unknown-linux-gnu
+rustup component add clippy --toolchain nightly-x86_64-unknown-linux-gnu
+
+cargo +nightly install cargo-watch
+cargo +nightly install evcxr
+
