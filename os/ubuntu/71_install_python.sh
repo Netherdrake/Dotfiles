9624b73b2bacfe2c790b7296afd323ba0cd0a1bf
Netherdrake <user.email>
2023-01-29 03:12:42 +0100

    Python 3.11

---
+++ os/ubuntu/71_install_python.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+
+sudo apt install python3.11 python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm -y
+
+python3.11 -m pip install --upgrade pip
