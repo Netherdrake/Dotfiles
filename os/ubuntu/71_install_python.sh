26de54881567b003ba82cebf65d5ae12bf73a827
Netherdrake <user.email>
2023-01-29 03:22:14 +0100

    Fix pyright

--- os/ubuntu/71_install_python.sh
+++ os/ubuntu/71_install_python.sh
@@ -4,3 +4,6 @@ sudo apt update
 sudo apt install python3.11 python3.11-dev python3.11-venv python3.11-distutils python3.11-gdbm -y
 
 python3.11 -m pip install --upgrade pip
+
+# microsoft's language server for Python
+pip3 install -U pyright
