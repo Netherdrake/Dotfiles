fe1c115e19205a2b93c1e85db3275b9ab8228862
user <user.email>
2024-02-08 21:53:02 +0100

    let rustup handle rust-analyzer

--- os/ubuntu/7_install_rust.sh
+++ os/ubuntu/7_install_rust.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-git clone https://github.com/rust-lang/rust-analyzer.git
-cd rust-analyzer
-cargo xtask install --server
-cd ..
-rm -rf rust-analyzer
+# git clone https://github.com/rust-lang/rust-analyzer.git
+# cd rust-analyzer
+# cargo xtask install --server
+# cd ..
+# rm -rf rust-analyzer
 
 rustup component add rust-src
 rustup component add rustfmt
 rustup component add clippy
+rustup component add rust-analyzer
 
 cargo install cargo-watch
 cargo install evcxr
 cargo install evcxr_repl
-
-rustup install nightly
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
-rustup component add rust-analyzer --toolchain nightly-x86_64-unknown-linux-gnu
-rustup component add clippy --toolchain nightly-x86_64-unknown-linux-gnu
-
-cargo +nightly install cargo-watch
-cargo +nightly install evcxr
 
 # benchmarking
 cargo install hyperfine
