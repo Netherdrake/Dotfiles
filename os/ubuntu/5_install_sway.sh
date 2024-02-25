1504a14c68ad1d33f91da6eb10daba5670488962
user.email <user.email>
2024-02-25 03:04:35 +0100

    add kanshi and a few more tweaks

--- os/ubuntu/5_install_sway.sh
+++ os/ubuntu/5_install_sway.sh
@@ -1,4 +1,4 @@
-sudo apt install -y pulseaudio-utils brightnessctl
+sudo apt install -y pulseaudio-utils brightnessctl playerctl
 sudo apt install -y sway swayidle waybar kanshi dunst rofi swaylock wdisplays grimshot
 sudo apt install -y imv
 mkdir -p ~/.config/sway
