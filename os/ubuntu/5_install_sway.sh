51239da92273ba50aa048e15df8ed345c008f6df
user <user.email>
2024-06-29 14:50:42 +0200

    add fuzzel

--- os/ubuntu/5_install_sway.sh
+++ os/ubuntu/5_install_sway.sh
@@ -1,5 +1,5 @@
 sudo apt install -y pulseaudio-utils brightnessctl playerctl
-sudo apt install -y sway swayidle waybar kanshi dunst rofi swaylock wdisplays grimshot cliphist
+sudo apt install -y sway swayidle waybar kanshi dunst rofi swaylock wdisplays grimshot cliphist fuzzel
 sudo apt install -y imv
 mkdir -p ~/.config/sway
 cp /etc/sway/config ~/.config/sway/
