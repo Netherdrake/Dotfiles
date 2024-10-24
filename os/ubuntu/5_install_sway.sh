fbffd8f2a11b2ebd7a4db5d280e6adf8aeb8d1ef
user <user.email>
2024-10-24 11:17:33 +0200

    add swaync

--- os/ubuntu/5_install_sway.sh
+++ os/ubuntu/5_install_sway.sh
@@ -1,5 +1,5 @@
 sudo apt install -y pulseaudio-utils brightnessctl playerctl
-sudo apt install -y sway swayidle waybar kanshi dunst rofi swaylock wdisplays grimshot cliphist fuzzel
+sudo apt install -y sway swayidle waybar kanshi sway-notification-center rofi swaylock wdisplays grimshot cliphist fuzzel
 sudo apt install -y imv
 mkdir -p ~/.config/sway
 cp /etc/sway/config ~/.config/sway/
